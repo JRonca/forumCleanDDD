9f13347483d406087e3df6ac94f4fa0babe28b9b
José Renato Ferreira Ronca <jrfr21112000@gmail.com>
2025-06-30 08:39:20 -0300

    feat: answer entitie, tests, repositories, object value slug

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest';
+import { Answer } from "../entities/answer";
+import { AnswersRepository } from '../repositories/answers-repository';
+import { AnswerQuestionUseCase } from './answer-question';
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+
+test('create an answer', async () => {
+  const sut = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await sut.execute({
+    instructorId: '1',
+    questionId: '1',
+    content: 'This is an answer',
+  });
+
+  expect(answer.content).toBe('This is an answer');
+})
